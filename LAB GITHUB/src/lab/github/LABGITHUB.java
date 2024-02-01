56330bf442a83b76fba861c5cb43be7004759c9d
SARA <ssaaasssarreer@gmail.com>
2024-02-01 21:42:39 +0300

    hello github community :)

---
+++ LAB GITHUB/src/lab/github/LABGITHUB.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.github;
+
+/**
+ *
+ * @author saraa
+ */
+public class LABGITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("hello github community :)");
+        
+        // TODO code application logic here
+    }
+    
+}
